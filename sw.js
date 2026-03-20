8ec0a4b4ec39e89be30aaa390e26308175758f4b
ranjith22808 <ranjith22808@gmail.com>
2026-03-20 19:11:38 +0530

    added 3 files
    added To make the installation work, your folder should now look like this:
    
    index.html (The code I gave you in the previous message)
    
    sw.js (The service worker file you created)
    
    manifest.json (The code above)

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'family-diary-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
